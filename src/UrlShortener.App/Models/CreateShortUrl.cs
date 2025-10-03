fc2338fb3db70a1d09b4e0728e3b43a66ca3af47
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2025-10-02 22:06:43 -0300

    fix: max length ShortCode

--- src/UrlShortener.App/Models/CreateShortUrl.cs
+++ src/UrlShortener.App/Models/CreateShortUrl.cs
@@ -4,7 +4,7 @@ namespace UrlShortener.App.Models;
 
 public sealed class CreateShortUrl
 {
-    [MaxLength(10, ErrorMessage = "Short code must have a maximum of 10 characters.")]
+    [MaxLength(8, ErrorMessage = "Short code must have a maximum of 8 characters.")]
     public string? ShortCode { get; set; } = null;
 
     [Required(AllowEmptyStrings = false, ErrorMessage = "The Long URL field is required.")]
